a768b8847548fb9b92fe518409f0bc41340e0343
d-mega <51013505+d-mega@users.noreply.github.com>
2023-02-05 09:43:09 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label.setText("OK")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
