ac0d74987fe3e20b25eba731484f653168a4af86
d-mega <51013505+d-mega@users.noreply.github.com>
2023-02-05 10:07:25 +0300

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+import requests
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
@@ -10,10 +13,43 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.pushButton.clicked.connect(self.run)
+        self.getImage(self.createurl())
+        self.print_img(self.map_file)
 
-    def print_img(self, pic_name):
-        self.pixmap = QPixmap(pic_name)
-        self.label.setPixmap(self.pixmap)
+    def run(self):
+        print(1)
+
+    def initUI(self):
+        # self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def createurl(self, z=17):
+        return f"http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&z={z}&l=map"
+
+    def getImage(self, map_request):
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
